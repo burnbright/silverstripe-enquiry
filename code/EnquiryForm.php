1f61c2a0c804246139e91077468c50fb94c62f95
Jeremy Shipman <jeremy@burnbright.net>
2015-03-03 17:02:29 +1300

    Enquiry form improvements to allow customisations of email

--- code/EnquiryForm.php
+++ code/EnquiryForm.php
@@ -2,7 +2,12 @@
 
 class EnquiryForm extends Form{
 
-	function __construct($controller,$name,$fields = null,$actions = null,$validator = null){
+	protected $emailtemplate = "EnquiryEmail";
+	protected $emailsubject;
+
+	protected $extraemaildata;
+
+	public function __construct($controller, $name, $fields = null, $actions = null, $validator = null){
 
 		if(!$fields)
 			$fields = new FieldList(
@@ -15,37 +20,68 @@ class EnquiryForm extends Form{
 			$actions = new FieldList(
 				new FormAction("submitenquiry",_t("EnquiryForm.SUBMIT","Send Enquiry"))
 			);
-		parent::__construct($controller,$name,$fields,$actions,$validator);
+		parent::__construct($controller ,$name, $fields, $actions, $validator);
 		
 		if(class_exists('SpamProtectorManager')) {
 			$this->enableSpamProtection();
 		}
 	}
 
-	function submitenquiry($data,$form){
+	public function submitenquiry($data,$form){
 
 		$siteconfig = SiteConfig::current_site_config();
 		$to = Email::getAdminEmail();
-		$subject = _t("EnquiryForm.SUBJECT","Website Contact");
-		//$data = new ArrayData($data);
+		$subject = $this->emailsubject ? $this->emailsubject :
+			_t("EnquiryForm.SUBJECT","Website Contact");
 		$form->makeReadOnly();
-		$email = new Email($to,$to,$subject,$this->customise(array('Values' => $form->Fields()))->renderWith('EnquiryEmail'));
-		if(isset($data['Email']) && Email::is_valid_address($data['Email']))
+
+		$data = array(
+			'Values' => $form->Fields()
+		);
+		if($this->extraemaildata){
+			$data = array_merge($data, $this->extraemaildata);
+		}
+		$email = new Email(
+			$to, $to, $subject,
+			$this->customise($data)->renderWith($this->emailtemplate)
+		);
+		if(
+			isset($data['Email']) && 
+			Email::is_valid_address($data['Email'])
+		){
 			$email->replyTo($data['Email']);
+		}
 		$success = $email->send();
 
-		$defaultmessage = "<p class=\"message good\">"._t("Enquiry.SUCCESS","Thanks for your contact. We'll be in touch shortly.")."</p>";
-		$content = ($siteconfig && $siteconfig->EnquiryContent) ? $siteconfig->EnquiryContent : $defaultmessage ;
+		$defaultmessage = "<p class=\"message good\">".
+			_t("Enquiry.SUCCESS",
+				"Thanks for your contact. We'll be in touch shortly."
+			).
+		"</p>";
+		$content = ($siteconfig && $siteconfig->EnquiryContent) ?
+						$siteconfig->EnquiryContent : $defaultmessage ;
 
 		if(Director::is_ajax()){
 			return "success";
 		}
 
-		//TODO: submit to a new "Page" pagetype
-		return array(
-				'Title' => _t('Enquiry.Singular','Enquiry'),
-				'Content' => $content
-		);
+		return new Page_Controller(new Page(array(
+			'Title' => _t('Enquiry.Singular','Enquiry'),
+			'Content' => $content,
+			'EnquiryForm' => ''
+		)));
+	}
+
+	public function setEmailTemplate($template) {
+		$this->emailtemplate = $template;
+	}
+
+	public function setEmailSubject($subject) {
+		$this->emailsubject = $subject;
+	}
+
+	public function setExtraEmailData($data) {
+		$this->extraemaildata = $data;
 	}
 
 }
--- templates/email/EnquiryEmail.ss
+++ templates/email/EnquiryEmail.ss
@@ -1,6 +1,6 @@
 <p>The following was submitted from your website enquiry form:</p>
 <% loop Values %>
 	<% if Value %><% if IsHidden %><% else %>
-		<p><strong>$Name:</strong> $Value</p>
+		<p><strong>$Title:</strong> $Value</p>
 	<% end_if %><% end_if %>
 <% end_loop %>
